39fc95ad20a1c9821805a58d6f5f3c0847392eb3
JohnChou <2922462750@qq.com>
2024-06-11 21:57:26 +0800

    1.增加了显示实时时间功能

--- src/main.cpp
+++ src/main.cpp
@@ -1,110 +1,130 @@
-// Demonstrates using Parola to display text and graphics in different
-// zones simultaneously.
-//
-// MD_MAX72XX library can be found at https://github.com/MajicDesigns/MD_MAX72XX
-//
-
+/**
+ * @file main.cpp
+ * @author 周晨阳 (2922462750@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-06-11
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
 #include <SPI.h>
-
+#include <WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 // Define the number of devices we have in the chain and the hardware interface
 // NOTE: These pin numbers will probably not work with your hardware and may
 // need to be adapted
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define MAX_DEVICES 4
-#define CLK_PIN   2
-#define DATA_PIN  3
-#define CS_PIN    4
+#define CLK_PIN 2
+#define DATA_PIN 3
+#define CS_PIN 4
 
+// 替换成你的WiFi网络名称和密码
+const char *ssid = "TP-LINK_8C30";
+const char *password = "z888888889";
+
+// 配置NTP客户端
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "ntp.aliyun.com", 28800, 60000);
 // HARDWARE SPI
 MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 // SOFTWARE SPI
-//MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+// MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
 MD_MAX72XX *pM;
 
 // Display parameters
-const uint8_t SCROLL_SPEED = 50;    // text speed
-const textEffect_t SCROLL_EFFECT = PA_SCROLL_LEFT;
+const uint8_t SCROLL_SPEED = 50; // text speed
+const textEffect_t SCROLL_EFFECT = PA_NO_EFFECT;
 const textPosition_t SCROLL_ALIGN = PA_CENTER;
-const uint16_t SCROLL_PAUSE = 0;    // in milliseconds
+const uint16_t SCROLL_PAUSE = 0; // in milliseconds
 
-const uint32_t BALL_MOVE_DELAY = 50;   // in milliseconds
+const uint8_t TEXT_ZONE = 0; // just change this to adjust display
+const uint8_t TXT_LOWER = 0;
+const uint8_t TXT_UPPER = 3;
 
-const uint8_t TEXT_ZONE = 0;    // just change this to adjust display
-// Define other dependent zone constants
-const uint8_t GRAPHIC_ZONE = (TEXT_ZONE == 0 ? 1 : 0);
-const uint8_t ZONE_BOUNDARY = 3;
-const uint8_t GRA_LOWER = (GRAPHIC_ZONE == 1 ? ZONE_BOUNDARY : 0);
-const uint8_t GRA_UPPER = (GRAPHIC_ZONE == 1 ? MAX_DEVICES - 1 : ZONE_BOUNDARY - 1);
-const uint8_t TXT_LOWER = (TEXT_ZONE == 1 ? ZONE_BOUNDARY : 0);
-const uint8_t TXT_UPPER = (TEXT_ZONE == 1 ? MAX_DEVICES - 1 : ZONE_BOUNDARY - 1);
-
-
-char message[] = { "Bouncing Ball" };
-
-void bounceBall(void)
-{
-  static bool bInitialized = false;
-  static uint16_t ballX, ballY;
-  static int8_t deltaX = 1, deltaY = 1;
-  static uint32_t prevTime = 0;
-  static uint16_t lower, upper;
-
-  if (!bInitialized)    // one time initialization of zone parameters
-  {
-    P.getDisplayExtent(GRAPHIC_ZONE, lower, upper);
-
-    ballX = lower;
-    ballY = 0;
-    
-    bInitialized = true;
-  }
-  
-  // Move the ball if its time to do so
-  if (millis() - prevTime <= BALL_MOVE_DELAY)
-    return;
-
-  prevTime = millis();
-
-  // block display updates for graphic zone modules
-  for (uint8_t m = GRA_LOWER; m <= GRA_UPPER; m++)
-    pM->control(m, MD_MAX72XX::UPDATE, MD_MAX72XX::OFF);
-
-  // work it all out here
-  pM->setPoint(ballY, ballX, false);     // take the old ball out
-
-  // new ball positions
-  ballX += deltaX;
-  ballY += deltaY;
-
-  // check for edge collisions
-  if (ballX == upper || ballX == lower)   // end bounce
-    deltaX *= -1;
-
-  if (ballY == 0 || ballY == ROW_SIZE - 1)  // top/bottom bounce
-    deltaY *= -1;  // top bounce
-
-  pM->setPoint(ballY, ballX, true);   // put the new ball in
-
-  // finally re-enable the blocked modules
-  for (uint8_t m = GRA_LOWER; m <= GRA_UPPER; m++)
-    pM->control(m, MD_MAX72XX::UPDATE, MD_MAX72XX::ON);
-}
+char message[] = {"08:00:00:00:00:00"};
 
 void setup(void)
 {
-  P.begin(2);
+  Serial.begin(115200);
+  P.begin();
   P.setZone(TEXT_ZONE, TXT_LOWER, TXT_UPPER);
-  P.setZone(GRAPHIC_ZONE, GRA_LOWER, GRA_UPPER);
-  P.displayZoneText(TEXT_ZONE, message, SCROLL_ALIGN, SCROLL_SPEED, SCROLL_PAUSE, SCROLL_EFFECT, SCROLL_EFFECT);
   P.setIntensity(15);
   pM = P.getGraphicObject();
-}
 
+  delay(100);
+
+  // 连接WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // 初始化NTP客户端
+  timeClient.begin();
+  // 更新时间
+  timeClient.update();
+
+  // 获取当前时间
+  String formattedTime = timeClient.getFormattedTime();
+  strcpy(message, formattedTime.substring(0, 5).c_str());
+  P.displayZoneText(TEXT_ZONE, message, SCROLL_ALIGN, SCROLL_SPEED, SCROLL_PAUSE, SCROLL_EFFECT, SCROLL_EFFECT);
+
+  // 输出当前时间
+  Serial.println(formattedTime);
+  P.displayAnimate();
+}
+// 上次更新时间的时间戳
+unsigned long lastUpdateTime = 0;
+// 定义呼吸灯周期和亮暗程度
+const int breathePeriod = 2000; // 2 秒钟一个周期
+const int minBrightness = 3;
+const int maxBrightness = 15;
 void loop(void)
 {
-    if (P.displayAnimate())
-      P.displayReset();
-    bounceBall();
+  // bounceBall();
+  P.displayAnimate();
+  // 计算当前时间在呼吸周期内的百分比
+  unsigned long currentMillis = millis();
+  float percentage = (float)(currentMillis % breathePeriod) / breathePeriod;
+
+  // 根据百分比计算当前亮度
+  int brightness;
+  if (percentage < 0.5)
+  { // 从暗到亮
+    brightness = minBrightness + (maxBrightness - minBrightness) * 2 * percentage;
+  }
+  else
+  { // 从亮到暗
+    brightness = maxBrightness - (maxBrightness - minBrightness) * 2 * (percentage - 0.5);
+  }
+
+  // 设置 LED 亮度
+  P.setIntensity(brightness);
+
+  // 延迟 10 毫秒,让呼吸效果更平滑
+  delay(10);
+  if (millis() - lastUpdateTime >= 60000)
+  {
+    // 更新时间
+    timeClient.update();
+
+    // 获取当前时间
+    String formattedTime = timeClient.getFormattedTime();
+
+    // 输出当前时间
+    Serial.println(formattedTime);
+    strcpy(message, formattedTime.substring(0, 5).c_str());
+    P.displayReset();
+
+    // 更新上次更新时间的时间戳
+    lastUpdateTime = millis();
+  }
 }
